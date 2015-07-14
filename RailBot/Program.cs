8ed6e7940edfd088c84af0e660330566a58861a1
Simone Capodicasa <simone.capo+git@gmail.com>
2015-07-14 21:45:52 +0200

    Ora scarico in qualche modo i dati dal bot

--- RailBot/Program.cs
+++ RailBot/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
 
 namespace RailBot
 {
 	class MainClass
 	{
+
+		const string tokenurl = "https://api.telegram.org/bot112284476:AAExtPGiiA1cmPf8hZLLiBRI29Sv5rr528g/";
+		const string geturl = tokenurl + "getUpdates?offset=:offset?offset=:offset:&timeout=60:&timeout=60";
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
-			Console.ReadLine ();
+			WebClient wc = new WebClient ();
+			while (true) {
+				var page = wc.DownloadString (geturl);
+				Console.WriteLine (page);
+				Thread.Sleep (2000);
+			}
 		}
 	}
 }
